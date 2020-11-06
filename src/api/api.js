5d9819d3ef72648a45799ce087687bc03925711e
Silvia <hxiaoyin@hotmail.com>
2020-11-07 01:23:01 +1100

    fallback to http://localhost:7001 for develop environment

--- src/api/api.js
+++ src/api/api.js
@@ -3,10 +3,9 @@ import axios from 'axios';
 import useAuth from './useAuth';
 
 const { REACT_APP_API_BASE_URL } = process.env;
-console.log(REACT_APP_API_BASE_URL);
 
 const instance = axios.create({
-  baseURL: REACT_APP_API_BASE_URL,
+  baseURL: REACT_APP_API_BASE_URL || 'http://localhost:7001',
 });
 
 useAuth(instance);
